55c110c8d357fece382e734d19aae92bf2c6d770
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-12-11 15:18:37 +0100

    Adds some comments

--- day-1/part-2/Program.fs
+++ day-1/part-2/Program.fs
@@ -4,11 +4,11 @@
 let main args =
     let slidingWindowSumIncreaseCount =
         File.ReadLines args.[1]
-        |> Seq.map (fun line -> line |> int)
-        |> Seq.windowed 3
-        |> Seq.map (fun arr -> arr |> Array.sum)
-        |> Seq.pairwise
-        |> Seq.filter (fun (a, b) -> b > a)
+        |> Seq.map (fun line -> line |> int) // parse input lines to ints
+        |> Seq.windowed 3 // divide up ints into sliding windows of size 3
+        |> Seq.map (fun arr -> arr |> Array.sum) // sum values in each sliding window
+        |> Seq.pairwise // create pairs of sums for comparison
+        |> Seq.filter (fun (a, b) -> b > a) // keep only increasing pairs of sums 
         |> Seq.length
     
     printfn "%d" slidingWindowSumIncreaseCount
