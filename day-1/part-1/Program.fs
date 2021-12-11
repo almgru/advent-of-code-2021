99a1f3076cf21c561fd6d013e5ba502487a00e8b
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-12-11 12:13:19 +0100

    Adds solution to day 1, part 1

---
+++ day-1/part-1/Program.fs
@@ -0,0 +1,15 @@
+﻿// Usage: dotnet run Program.fs ../input.txt
+open System.IO
+
+[<EntryPoint>]
+let main args =
+    let increaseCount =
+        File.ReadLines args.[1]
+        |> Seq.map (fun str -> str |> int)
+        |> Seq.pairwise
+        |> Seq.filter (fun (a, b) -> b > a)
+        |> Seq.length
+
+    printfn "Number of increases: %d" increaseCount
+
+    0
