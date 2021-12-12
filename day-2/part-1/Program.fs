5c748d2664163a4db6b2352f824676580e4dcd8d
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-12-12 10:21:42 +0100

    Adds solution to day 2, part 1

---
+++ day-2/part-1/Program.fs
@@ -0,0 +1,42 @@
+﻿open System.IO
+
+type Command = {
+    Name: string 
+    Value: int
+}
+
+type Position = {
+    HorizontalPosition: int
+    Depth: int
+}
+
+[<EntryPoint>]
+let main args =
+    let finalPosition =
+        File.ReadLines args.[1]
+        |> Seq.map (fun line -> (line.Trim ()).Split " ")
+        |> Seq.map (fun line -> {
+            Name = line |> Seq.head
+            Value = line |> Seq.last |> int})
+        |> Seq.fold
+            (fun pos command ->
+                match command.Name.ToLower () with
+                | "forward" -> {
+                    HorizontalPosition = pos.HorizontalPosition + command.Value
+                    Depth = pos.Depth }
+                | "up" -> {
+                    HorizontalPosition = pos.HorizontalPosition
+                    Depth = pos.Depth - command.Value }
+                | "down" -> {
+                    HorizontalPosition = pos.HorizontalPosition
+                    Depth = pos.Depth + command.Value }
+                | _ -> raise (System.ArgumentException ()))
+            { HorizontalPosition = 0; Depth = 0 }
+    
+    printfn
+        "Horizontal position %d multiplied by depth %d = %d"
+        finalPosition.HorizontalPosition
+        finalPosition.Depth
+        (finalPosition.HorizontalPosition * finalPosition.Depth)
+
+    0
