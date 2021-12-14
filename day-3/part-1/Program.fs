b31584f76cd5126f8094bfd7c1258c7accb53e36
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-12-14 04:05:58 +0100

    Adds solution to day 3, part 1

--- day-3/part-1/Program.fs
+++ day-3/part-1/Program.fs
@@ -1,4 +1,37 @@
-﻿
+﻿open System.IO
+open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+[<EntryPoint>]
+let main args =
+    let input =
+        File.ReadAllLines args.[1]
+        |> Array.map (fun line -> (line.Trim ()).ToCharArray ())
+        |> Array.transpose
+    
+    let gammaRate =
+        input
+        |> Array.map (fun arr ->
+            arr
+            |> Array.countBy id
+            |> Array.maxBy (fun tup -> snd tup) 
+            |> fst)
+        |> String 
+        |> (fun str -> Convert.ToInt32 (str, 2))
+    
+    let epsilonRate =
+        input
+        |> Array.map (fun arr ->
+            arr
+            |> Array.countBy id
+            |> Array.minBy (fun tup -> snd tup)
+            |> fst)
+        |> String 
+        |> (fun str -> Convert.ToInt32 (str, 2))
+    
+    printfn
+        "gamma rate %d x epsilon rate %d = power consumption %d"
+        gammaRate
+        epsilonRate
+        (gammaRate * epsilonRate)
+
+    0
